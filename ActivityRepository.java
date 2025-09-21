3563aeb015a9cfa338ca52a1d65c4552cf178264
Prachi Pratyasha Mishra <83776034+PrachiPMishra@users.noreply.github.com>
2025-09-21 21:13:00 +0530

    Create ActivityRepository.java

---
+++ ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.example.carbontracker.repository;
+
+import com.example.carbontracker.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    List<Activity> findByUserId(Long userId);
+}
